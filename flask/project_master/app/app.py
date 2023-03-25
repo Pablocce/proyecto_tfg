32f1677e40d1fd22613a6f73f83164446463c73e
Pablo <Pablocalvoespinar04@gmail.com>
2023-03-25 15:30:18 +0100

    arreglado css no cargaba
    commit by pablusito

--- flask/project_master/app/app.py
+++ flask/project_master/app/app.py
@@ -33,6 +33,6 @@ def pacientesCargarTabla():
 
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0', port=8000) 
+    app.run(debug =True, host='0.0.0.0', port=8000) 
     #el atributo debug mode sirve para definir el tipo de depuración,
     #si es true podremos modificar el código sin reiniciar el script
