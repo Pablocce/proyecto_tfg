117deca2839a6258e925a26d81e4ff7699420d77
Pablo <Pablocalvoespinar04@gmail.com>
2023-03-26 15:48:28 +0200

    cambios hmtls

--- flask/project_master/app/app.py
+++ flask/project_master/app/app.py
@@ -13,6 +13,10 @@ def index():
 def agenda():
     return "<p> Diego tonto <p>"
 
+@app.route('/empleados')
+def empleados():
+    return render_template("empleados.html")
+
 @app.route('/pacientes')
 def servicios():
     datos = pacientesCargarTabla()
@@ -33,6 +37,6 @@ def pacientesCargarTabla():
 
 
 if __name__=='__main__':
-    app.run(debug =True, host='0.0.0.0', port=8000) 
+    app.run(host='0.0.0.0', port=8000) 
     #el atributo debug mode sirve para definir el tipo de depuración,
     #si es true podremos modificar el código sin reiniciar el script
