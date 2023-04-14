3e8248ca29b63806b81296aaa74653f17857811f
marcuelo <113529909+marcuelo@users.noreply.github.com>
2023-04-14 12:02:26 +0200

    Add files via upload

---
+++ pedidos.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Gestión de Pedidos</title>
+  </head>
+  <body>
+    <h1>Gestión de Pedidos</h1>
+    <form action="##el codigo##" method="post">
+      <label for="id_pedido">ID del pedido:</label>
+      <input type="text" id="id_pedido" name="id_pedido" required>
+      <br>
+      <label for="nombre">Nombre:</label>
+      <input type="text" id="nombre" name="nombre" required>
+      <br>
+      <label for="dni">DNI:</label>
+      <input type="text" id="dni" name="dni" required>
+      <br>
+      <input type="submit" value="Realizar Pedido">
+    </form>
+  </body>
+</html>
