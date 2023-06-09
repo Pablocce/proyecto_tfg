06207b16d812cb16c96cf689f6307f8d66a3be9b
Armandoarmandoarmando <124271270+Armandoarmandoarmando@users.noreply.github.com>
2023-06-09 12:52:03 +0200

    Add files via upload

--- proyecto_tfg-main(3)/proyecto_tfg-main/flask/project_master/clinicadental/app.py
+++ proyecto_tfg-main(3)/proyecto_tfg-main/flask/project_master/clinicadental/app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, flash, redirect, url_for, request
-from forms import RegistrationForm, LoginForm, ChangePasswordForm, NewEmployeeForm,NewPedidoForm
+from forms import NewPacienteForm, RegistrationForm, LoginForm, ChangePasswordForm, ChangeProductPrice,NewEmployeeForm,NewOrderWarehouse,EditEmployeeForm, NewEmployeeSchedule, DeleteEmployee, NewCitasFrom,NewProductWarehouse
 from flask_login import LoginManager
 from flask_login import login_user, current_user, UserMixin, logout_user
 from time import sleep
+from datetime import date
 
 import psycopg2
 
@@ -63,7 +64,7 @@ def gestion():
                 emp_surname = formEmployee.emp_surname.data
                 emp_salary = formEmployee.emp_salary.data
                 emp_user_id = 0
-                cur.execute("INSERT INTO public.employees (emp_name, emp_surname, emp_salary) values (%s,%s,%s)",(emp_name,emp_surname,emp_salary))
+                cur.execute("INSERT INTO public.employees (emp_name, emp_surname, emp_salary) values (%s,%s,%s,%s)",(emp_name,emp_surname,emp_salary,emp_user_id))
                 #flash(f'Cuenta creada para: {formEmployee.username.data}!', 'success')
                 cur.close()
                 conn.commit()
@@ -85,6 +86,107 @@ def gestion():
             return "<p>No puedes ver esto por falta de permisos</p>"
     else:
         return render_template('not_authenticated.html')
+
+
+def obtain_users():
+    conn = conector()
+    cur = conn.cursor()
+    cur.execute("SELECT id_user, username FROM users")
+    users_data = cur.fetchall()
+    cur.close()
+    conn.close()
+    return users_data
+
+def obtain_schedules():
+    conn = conector()
+    cur = conn.cursor()
+    cur.execute("SELECT emp_schedule.schedule_id, emp_schedule.day, employees.emp_name, emp_schedule.start_time, emp_schedule.end_time FROM emp_schedule JOIN employees ON emp_schedule.id_employee = employees.id_employee ORDER BY CASE emp_schedule.day WHEN 'lunes' THEN 1 WHEN 'martes' THEN 2 WHEN 'miércoles' THEN 3 WHEN 'jueves' THEN 4 WHEN 'viernes' THEN 5 ELSE 6 END, TO_TIMESTAMP(emp_schedule.start_time, 'HH24:MI')")
+    data = cur.fetchall()
+    cur.close()
+    conn.close()
+    return data
+
+def search_fields_employee(id):
+    conn = conector()
+    cur = conn.cursor()
+    cur.execute("SELECT emp_name, emp_surname, emp_salary, emp_user_id FROM employees WHERE id_employee = %s", (id,))
+    employee_data = cur.fetchone()
+    cur.close()
+    conn.close()
+    return list(employee_data)
+
+
+@app.route('/modifyemp/<id>', methods=['GET', 'POST'])
+def modify_emp(id):
+    formEmployee = EditEmployeeForm()
+    datos_empleado = search_fields_employee(id)
+    if current_user.is_authenticated:
+        if current_user.is_admin:
+            if request.form:
+                conn = conector()
+                cur = conn.cursor()
+                emp_name = formEmployee.emp_name.data
+                emp_surname = formEmployee.emp_surname.data
+                emp_salary = formEmployee.emp_salary.data
+                cur.execute("UPDATE employees set emp_name = %s, emp_surname = %s, emp_salary = %s where id_employee = %s",(emp_name, emp_surname, emp_salary, id))
+                cur.close()
+                conn.commit()
+                conn.close()
+                return redirect(url_for('gestion'))
+        else:
+            return "<p>No puedes ver esto por falta de permisos</p>"
+    else:
+        return render_template('not_authenticated.html')
+    return render_template('modifyemp.html', id = id, datos_empleado = datos_empleado, formEmployee = formEmployee)
+
+@app.route('/deleteemp/<id>', methods=['GET','POST'])
+def delete_emp(id):
+    conn = conector()
+    cur = conn.cursor()
+    cur.execute("DELETE FROM employees WHERE id_employee = %s", (id,))
+    cur.close()
+    conn.commit()
+    conn.close()
+    return redirect(url_for('gestion'))
+
+
+@app.route('/schedule/<id>', methods=['GET','POST'])
+def schedule_emp(id):
+    formEmployee = NewEmployeeSchedule()
+    conn = conector()
+    cur = conn.cursor()
+    cur.execute("SELECT emp_name from employees where id_employee = %s",(id,))
+    employee_name = cur.fetchmany()
+    employee_name = employee_name[0]
+    cur.execute("SELECT schedule_id, id_employee, day, start_time, end_time FROM emp_schedule WHERE id_employee = %s ORDER BY CASE day WHEN 'lunes' THEN 1 WHEN 'martes' THEN 2 WHEN 'miércoles' THEN 3 WHEN 'jueves' THEN 4 WHEN 'viernes' THEN 5 ELSE 6 END", (id,))
+    employee_schedule = cur.fetchall()  
+    cur.close()
+    conn.close()
+    if request.form:
+        conn = conector()
+        cur = conn.cursor()
+        day = formEmployee.schedule_day.data 
+        entry = formEmployee.schedule_entry.data
+        exit = formEmployee.schedule_exit.data
+        cur.execute("INSERT INTO public.emp_schedule (id_employee, day, start_time, end_time) VALUES(%s,%s,%s,%s)",(id,day,entry,exit))
+        cur.close()
+        conn.commit()
+        conn.close()
+        return redirect(f'/schedule/{id}') 
+    return render_template('schedule.html',formEmployee = formEmployee, employee_schedule = employee_schedule, employee_name = employee_name)
+
+@app.route('/schedule_delete/<id>')
+def schedule_delete(id):
+    conn = conector()
+    cur = conn.cursor()
+    cur.execute("select id_employee from emp_schedule where schedule_id = %s",(id,))
+    id_user = cur.fetchone
+    cur.execute("delete from emp_schedule where schedule_id = %s",(id,))
+    cur.close() 
+    conn.commit()
+    conn.close()
+    return redirect('/gestion') 
+
 
 
 
@@ -180,39 +282,132 @@ def account():
         conn.close()
     return render_template('account.html', title='Account', form=form)
 
-@app.route("/pedidos")
+@app.route("/pedidos", methods=['GET', 'POST'])
 def pedidos(): 
-    conn = conector()
-    form = NewPedidoForm()
+    if current_user.is_authenticated:
+        conn = conector()
+        formNewOrder = NewOrderWarehouse()
+        formNewProduct = NewProductWarehouse()
+        formChangePrice = ChangeProductPrice()
 
-    cur = conn.cursor()
-
-
-    #select para mostrar los pedidos
-    cur.execute("SELECT id_pedido,nombreProduc,precUnidad,cantidad,precioTotalUnidad,fecha,nombre_empresa from pedidos p join supplier su on p.id_empresa=su.id_empresa join product produ on p.id_producto=produ.id_producto")
-    user_data = cur.fetchall()
-    
-    print(user_data)
+        cur = conn.cursor()
 
 
-    # select para empresa
-    cur.execute("SELECT * from supplier")
-    empresa=cur.fetchall()
-    
-
-    # select para productos
-    cur.execute("SELECT * from product")
-    productos=cur.fetchall()
-    
+        #select para mostrar los pedidos
+        cur.execute("SELECT id_pedido,nombreProduc,precUnidad,p.cantidad,precioTotalUnidad,fecha,nombre_empresa, (select emp_name from employees e join Users u on e.emp_user_id= u.id_user join pedidos p on p.id_usuario=u.id_user ) from pedidos p join supplier su on p.id_empresa=su.id_empresa join product produ on p.id_producto=produ.id_producto")
+        user_data = cur.fetchall()
+        
 
 
+        # select para empresa
+        cur.execute("SELECT * from supplier")
+        empresas_data=cur.fetchall()
+        
 
-    
-    cur.close()
-    conn.close()
-    return render_template("pedidos.html",pedidos=user_data)
+        # select para productos
+        cur.execute("SELECT product.id_producto,  product.precUnidad,  product.nombreProduc,  product.id_empresa,product.cantidad, supplier.nombre_empresa from product join supplier on supplier.id_empresa = product.id_empresa")
+        productos=cur.fetchall()
+  
+        cur.close()
+        conn.close()
+        if request.method == 'POST':
+            conn = conector()
+            cur = conn.cursor()
+            proveedor_seleccionado = request.form.get('proveedor')
+            producto_seleccionado = request.form.get('producto')
+            cur.execute("select precUnidad from product where id_producto = %s",(producto_seleccionado,))
+            precio_unitario = cur.fetchone()[0]
+
+            cur.execute("select id_employee from employees where emp_user_id = %s",(current_user.get_id()))
+            id_employee = cur.fetchone()
+            print(id_employee)
+            cur.execute("INSERT INTO pedidos (id_producto, cantidad, fecha, preciototalunidad, id_empresa, employee_id) VALUES (%s, %s, %s, %s, %s, %s);",(producto_seleccionado, formNewOrder.product_qty.data, date.today(), float(precio_unitario), proveedor_seleccionado, id_employee))
+            #flash(f'Cuenta creada para: {formEmployee.username.data}!', 'success')
+            cur.close()
+            conn.commit()
+            conn.close()
+        return render_template("pedidos.html",title ='Pedidos',pedidos=user_data, productos = productos, empresas_data = empresas_data, formNewOrder = formNewOrder,NewProductWarehouse = formNewProduct,formChangePrice = formChangePrice)
+    else:
+        return "identificate"
+
+
+@app.route("/citas", methods=['GET', 'POST'])
+def citas(): 
+    if current_user.is_authenticated:
+        conn = conector()
+        formNewOrder = NewCitasFrom()
+
+        cur = conn.cursor()
+
+
+        #select para mostrar citas
+        cur.execute("SELECT c.id_cita,c.fecha, c.descripcion,p.dni,p.nombre,p.apellido1 from pacientes p join cita c on p.dni=c.id_paciente order by fecha;")
+
+        user_data = cur.fetchall()
+        print (user_data)
+
+        cur.close()
+        conn.commit()
+        conn.close()
+        return render_template('citas.html', title='Cita', citas=user_data, formNewOrder=formNewOrder)
+
+@app.route("/pacientes", methods=['GET', 'POST'])
+def pacientes(): 
+    if current_user.is_authenticated:
+        conn = conector()
+        formNewOrder = NewPacienteForm()
+
+        cur = conn.cursor()
+
+
+        #select para mostrar citas
+        cur.execute("SELECT p.dni,p.nombre,p.apellido1,p.apellido2, p.id_cita from pacientes p")
+        user_data = cur.fetchall()
+        print (user_data)
+
+        cur.close()
+        conn.commit()
+        conn.close()
+        return render_template('pacientes.html', title='Pacientes', pacientes=user_data, formNewOrder=formNewOrder)
+
+
 
 
 if __name__=='__main__':
     app.run(host='0.0.0.0')
 
+
+
+@app.route("/citas", methods=['GET', 'POST'])
+def citas(): 
+    # Verifica si el usuario actual está autenticado
+    if current_user.is_authenticated:
+        # Conexión a la base de datos
+        conn = conector()
+
+        # Creación del formulario para una nueva cita
+        formNewOrder = NewCitasFrom()
+
+        # Creación del cursor para ejecutar consultas SQL
+        cur = conn.cursor()
+
+        # Consulta SQL para obtener los datos de las citas y los pacientes
+        cur.execute("SELECT c.id_cita, c.fecha, c.descripcion, p.dni, p.nombre, p.apellido1 FROM pacientes p JOIN cita c ON p.dni = c.id_paciente ORDER BY fecha;")
+        
+        # Obtiene todos los datos de las citas y los pacientes
+        user_data = cur.fetchall()
+
+        # Imprime los datos obtenidos en la consola (para propósitos de depuración)
+        print(user_data)
+
+        # Cierra el cursor
+        cur.close()
+
+        # Guarda los cambios en la base de datos
+        conn.commit()
+
+        # Cierra la conexión a la base de datos
+        conn.close()
+
+        # Renderiza la plantilla 'citas.html' y pasa los datos de las citas, el formulario y el título como argumentos
+        return render_template('citas.html', title='Cita', citas=user_data, formNewOrder=formNewOrder)
--- proyecto_tfg-main(3)/proyecto_tfg-main/flask/project_master/clinicadental/forms.py
+++ proyecto_tfg-main(3)/proyecto_tfg-main/flask/project_master/clinicadental/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
 #tipo de dato y meter dominios dentro
-from wtforms import StringField , PasswordField, SubmitField, BooleanField, IntegerField
+from wtforms import StringField , PasswordField, SubmitField, BooleanField, IntegerField, DecimalField
 #importamos validadores
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, AnyOf, Regexp
 import email_validator
 import psycopg2
 
@@ -71,9 +71,62 @@ class NewEmployeeForm(FlaskForm):
 
     submit = SubmitField('Añadir empleado')
 
-class NewPedidoForm(FlaskForm):
-    username = StringField('Nombre usuario', validators=[DataRequired(), Length(min=5, max=20)])
-    password = PasswordField('Contraseña', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirmar contraseña', validators=[DataRequired(), EqualTo('password')])
-    codigo_admin = PasswordField('Código administrador', validators=[DataRequired()])
-    is_admin = BooleanField('Marcar como administrador?')
+class EditEmployeeForm(FlaskForm):
+    emp_name = StringField('Nombre empleado', validators=[DataRequired(), Length(max=20)])
+    emp_surname = StringField('Apellidos empleado', validators=[DataRequired(), Length(max=35)])
+    emp_salary = IntegerField('Salario empleado', validators=[DataRequired()])
+    emp_user_id = IntegerField('ID usuario', validators=[])
+
+    submit = SubmitField('Añadir empleado')
+
+
+class DeleteEmployee(FlaskForm):
+    emp_id = IntegerField('ID empleado', validators=[DataRequired()])
+    
+    delete = SubmitField('Borrar empleado')
+
+class NewEmployeeSchedule(FlaskForm):
+    schedule_day = StringField('Día de la semana', validators=[DataRequired(),AnyOf(['lunes', 'martes', 'miércoles', 'jueves', 'viernes'],
+                                                message='El día debe ser lunes, martes, miércoles, jueves o viernes.')])
+    
+    schedule_entry = StringField('Hora de entrada', validators=[DataRequired()])
+    schedule_exit = StringField('Hora de salida', validators=[DataRequired()])
+
+    submit = SubmitField('Añadir horario')
+
+
+class NewProductWarehouse(FlaskForm):
+    product_name = StringField('Nombre del producto', validators=[DataRequired()])
+    product_price = DecimalField('Precio del producto', validators=[DataRequired()])
+    product_stock = IntegerField('Stock del producto', validators=[DataRequired()])
+    submit = SubmitField('Añadir producto')
+
+class ChangeProductPrice(FlaskForm):
+    product_new_price = DecimalField('Nuevo precio del producto', validators=[DataRequired()])
+
+
+class NewOrderWarehouse(FlaskForm):
+    product_qty = IntegerField('Cantidad del producto', validators=[DataRequired()])
+    submit = SubmitField('Crear pedido')
+    pass
+
+class NewCitasFrom(FlaskForm):
+    nom_doc=StringField('Nombre del doctor', validators=[DataRequired()])
+    nom_paciente=StringField('Nombre del paciente', validators=[DataRequired()])
+    ape1=StringField("Primer apellido",validators=[DataRequired()])
+    descripcion= StringField('Descripcion de la cita', validators=[DataRequired()])
+    fecha=StringField('Fecha de la cita', validators=[DataRequired()])
+
+    submit = SubmitField('Cita creada')
+
+
+class NewPacienteForm(FlaskForm):
+    dni=StringField("Dni del paciente",validators=[DataRequired()])
+    nombre=StringField("Nombre del paciente",validators=[DataRequired()])
+    ape1=StringField("Primer apellido",validators=[DataRequired()])
+    ape2=StringField("Segundo apelllido",validators=[DataRequired()])
+    id_cita=DecimalField("ID de la cita",validators=[DataRequired()])
+
+    submit = SubmitField('Paciente registrado')
+
+
